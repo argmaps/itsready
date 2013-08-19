980f505c8fd485b5c5317110ee0d0f29631e4491
Josh Weinstein <weinstein.josh@gmail.com>
2013-08-19 17:20:35 +0700

    Send new user directly to add customer page as they can't create notifications before they have customers.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         auto_login(@user, true)
 
         format.json { render :json, status: :ok }
-        format.html { redirect_to user_notifications_path(current_user) }
+        format.html { redirect_to user_customers_path(current_user) }
       else
         format.json { render :json => {error: "invalid username-password combination."}, :status => :unprocessable_entity }
         format.html { redirect_to :action => :new }
