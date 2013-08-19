34f0aa1a72dd65a56888d812120d13a9c48d23c4
Josh Weinstein <weinstein.josh@gmail.com>
2013-08-19 21:27:37 +0700

    Modified workflow for creating a notification. Now selecting a customer automatically creates a new notification for that customer. In order to do this, we hard code the notification message.

--- app/assets/javascripts/notifications.js
+++ app/assets/javascripts/notifications.js
@@ -1,3 +1,11 @@
-$('.notification_ready input[type=checkbox]').click(function() {
-    $(this).closest('form').submit();
+$(function() {
+    $('.notification_ready input[type=checkbox]').click(function() {
+        $(this).closest('form').submit();
+    });
+
+    $('#new_notification .dk_options_inner li').click(function() {
+        var selectedValue = $(this).find('a').attr('data-dk-dropdown-value');
+        $('#notification_customer_id').val(selectedValue);
+        $(this).closest('form').submit();
+    });
 });
