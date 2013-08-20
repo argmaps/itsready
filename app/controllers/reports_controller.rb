2d0af4d29b7fcb29508e7403cba0f58e612b28ae
Josh Weinstein <weinstein.josh@gmail.com>
2013-08-20 12:19:20 +0700

    Group reports into daily counts of total texts per customer per day.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,18 @@ class ReportsController < ApplicationController
 
   # GET /reports
   def index
-    @notifications = current_user.notifications.sent
+    @data = Array.new.tap do |r|
+      current_user.customers.each do |customer|
+        r << customer.notifications.sent.group_by {|n| n.sent_at.to_date}.map do |k,v|
+          {
+            date: k,
+            name: customer.full_name,
+            count: v.length,
+            country_code: customer.country_code,
+            phone: customer.phone
+          }
+        end
+      end
+    end.flatten!
   end
 end
